9114ea706a40b3fa4fb328f12591955c64bbb04c
Aritra <aritra041999.dev@gmail.com>
2024-02-25 23:42:45 +0100

    Add db connection and migration

---
+++ scout/models/setup.go
@@ -0,0 +1,29 @@
+package models
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/sqlite"
+	// "github.com/glebarez/sqlite" // Pure go SQLite driver, checkout https://github.com/glebarez/sqlite for details
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	var err error
+	DB, err = gorm.Open(sqlite.Open("scout.db"), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println("Cannot connect to database ")
+		log.Fatal("connection error:", err)
+	} else {
+		fmt.Println("We are connected to the database")
+	}
+}
+
+func MigrateTables() {
+	DB.AutoMigrate(&Document{})
+}
+
