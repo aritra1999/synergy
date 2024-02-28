718e68ed6ed39bcc9bd95384600817eb665d1f45
Aritra <aritra041999.dev@gmail.com>
2024-02-28 10:30:43 +0100

    Commented possible future endpoints

--- scout/server/server.go
+++ scout/server/server.go
@@ -17,9 +17,16 @@ func Init() *gin.Engine {
 	// router.Use(middlewares.AuthMiddleware())
 	
 	router.GET("/health", controllers.HealthController)
+
 	router.GET("/index/:id", controllers.GetIndexController)
 	router.POST("/index", controllers.PostIndexController)
+	
 	router.POST("/ingest", controllers.PostIngestController)
 	
+	// router.GET("/index", controllers.GetIndexController)
+	// router.PUT("/index/:id", controllers.PostIndexController)
+	// router.DELETE("/index/:id", controllers.PostIndexController)
+	// router.POST("/search", controllers.PostSearchController)
+	// router.POST("/backup", controllers.PostIngestController)
 	return router
 }
