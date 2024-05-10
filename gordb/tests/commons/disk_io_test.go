ab39af8261f8fbbdbb9f44677bb6f89d2c7836a0
Aritra <aritra041999.dev@gmail.com>
2024-05-10 13:12:06 +0530

    Update disk io logic to read from project root / project store

--- gordb/tests/commons/disk_io_test.go
+++ gordb/tests/commons/disk_io_test.go
@@ -4,32 +4,37 @@ import (
 	"encoding/json"
 	"gordb/commons"
 	"os"
+	"path/filepath"
 	"testing"
 
 	. "github.com/franela/goblin"
 )
 
 func TestDiskIo(t *testing.T) {
-	g := Goblin(t)
-	dummyObj := map[string]string{
-		"key": "value",
-	}
-	dummyContent, err := json.Marshal(dummyObj)
+	var (
+		g             = Goblin(t)
+		storePath     = commons.GetStorePath()
+		testStorePath = filepath.Join(storePath, "test")
+		dummyObj      = map[string]string{"key": "value"}
+	)
 
+	var dummyContent, err = json.Marshal(dummyObj)
 	if err != nil {
 		t.Errorf("Debug: %s", err)
 	}
 
 	var setUp = func() {
-		err := os.WriteFile("./test.json", dummyContent, 0644)
-		if err != nil {
+		if err := os.Mkdir(testStorePath, 0755); err != nil && !os.IsExist(err) {
+			t.Errorf("%s", err)
+		}
+
+		if err := os.WriteFile(filepath.Join(testStorePath, "test.json"), dummyContent, 0644); err != nil && !os.IsExist(err) {
 			t.Errorf("%s", err)
 		}
 	}
 
 	var tearDown = func() {
-		err := os.Remove("./test.json")
-		if err != nil {
+		if err := os.RemoveAll(testStorePath); err != nil {
 			t.Errorf("%s", err)
 		}
 	}
@@ -39,8 +44,19 @@ func TestDiskIo(t *testing.T) {
 		g.AfterEach(tearDown)
 
 		g.It("Should write to file", func() {
-			err := commons.WriteFile("./test.json", dummyObj)
+			var err = commons.WriteFile("test/test.json", dummyObj)
 			g.Assert(err).Equal(nil)
+		})
+
+		g.It("Should not append items, but replace the file content", func() {
+			var err = commons.WriteFile("test/test.json", dummyObj)
+			g.Assert(err).Equal(nil)
+			err = commons.WriteFile("test/test.json", dummyObj)
+			g.Assert(err).Equal(nil)
+
+			data, err := commons.ReadFile("test/test.json")
+			g.Assert(err).Equal(nil)
+			g.Assert(data).Equal(dummyContent)
 		})
 	})
 
@@ -49,10 +65,9 @@ func TestDiskIo(t *testing.T) {
 		g.AfterEach(tearDown)
 
 		g.It("Should read from file", func() {
-			data, err := commons.ReadFile("./test.json")
+			data, err := commons.ReadFile("test/test.json")
 			g.Assert(err).Equal(nil)
 			g.Assert(data).Equal(dummyContent)
 		})
 	})
-
 }
